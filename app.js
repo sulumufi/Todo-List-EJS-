fcbd64d3d56cd5fd642663d6083c0e4f28e0dadc
sulu mufi <suluksm@gmail.com>
2021-07-23 15:44:40 +0530

    Add GitIgnore, procFile, update Ports

--- app.js
+++ app.js
@@ -204,6 +204,13 @@ app.get("/about", function (req, res) {
     res.render("about");
 })
 
-app.listen(3000, function () {
-    console.log("server up and running!")
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
+
+app.listen(port, function () {
+    console.log("server started!")
 })
